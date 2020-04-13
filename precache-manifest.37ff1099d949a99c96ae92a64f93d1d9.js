317f08113419cfec38ed1a7b89765a5bc9582af4
srikanthmudili <srikanth512000@gmail.com>
2020-04-13 13:31:20 +0530

    Updates

---
+++ precache-manifest.37ff1099d949a99c96ae92a64f93d1d9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa1af564b715165c8d54a18c9540696",
+    "url": "/github.io/trivia/index.html"
+  },
+  {
+    "revision": "1726cdb04e0bff47091a",
+    "url": "/github.io/trivia/static/css/2.2990c7a7.chunk.css"
+  },
+  {
+    "revision": "7f1231ca4ddf0c539bde",
+    "url": "/github.io/trivia/static/css/main.122b75ee.chunk.css"
+  },
+  {
+    "revision": "1726cdb04e0bff47091a",
+    "url": "/github.io/trivia/static/js/2.430b8a27.chunk.js"
+  },
+  {
+    "revision": "86493497955f2c03ad4534f44ae20baa",
+    "url": "/github.io/trivia/static/js/2.430b8a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1231ca4ddf0c539bde",
+    "url": "/github.io/trivia/static/js/main.e5c0022f.chunk.js"
+  },
+  {
+    "revision": "6a57d6895ce1b04bfb51",
+    "url": "/github.io/trivia/static/js/runtime-main.6de5cf6a.js"
+  },
+  {
+    "revision": "817649970fb7e833865f6a01aed905da",
+    "url": "/github.io/trivia/static/media/datefact.81764997.png"
+  },
+  {
+    "revision": "385a4c5c456980d835a0e2a65ca6c910",
+    "url": "/github.io/trivia/static/media/numberfact.385a4c5c.png"
+  },
+  {
+    "revision": "5e7012663e7030111290f4d4c90a62bc",
+    "url": "/github.io/trivia/static/media/trivia.5e701266.png"
+  },
+  {
+    "revision": "a2aca36518e237a5e5da637a123c0234",
+    "url": "/github.io/trivia/static/media/yearfacts.a2aca365.png"
+  }
+]);
